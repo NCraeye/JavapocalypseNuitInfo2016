2dd31a35c6cdfb18d07678501dc6745be172d2e6
steway <steward.ouadi@hotmail.com>
2016-12-01 23:38:23 +0100

    début des pages home et info

--- ndi2016/index.php
+++ ndi2016/index.php
@@ -1,4 +1,19 @@
 <?php
-phpinfo();
+
+include 'navbar.php';
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+
+<div class="container">
+  <h3>Inverted Navbar</h3>
+  <p>An inverted navbar is black instead of gray.</p>
+  <img src="img/default_image.png" alt="Mountain View" style="width:304px;height:228px;">
+</div>
+
+</body>
+</html>
---
+++ ndi2016/info.php
@@ -0,0 +1,23 @@
+<?php
+
+include 'navbar.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+
+<div class="container">
+  <h2>Website links</h2>
+  <p>Solidarity guide in Paris : <a href="https://api-site.paris.fr/images/77130">link</a></p>
+  <p>Hostel of migrants's facebook group : <a href="https://www.facebook.com/LAuberge-des-Migrants-358496450338/?fref=ts">link</a></p>
+  <p>Gisti : <a href="http://www.gisti.org/spip.php?page=sommaire">link</a></p>
+  <p>Service to migrants platform : <a href="http://www.psmigrants.org/site/">link</a></p>
+  <p>MSF website : <a href="http://www.msf.fr/">link</a></p>
+
+</div>
+
+</body>
+</html>
